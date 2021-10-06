e2b73e373bf4c78dac75060080f71ca8469d24d5
George Sebik <waffilka@gmail.com>
2021-10-06 18:20:58 +0300

    routing map

--- src/app/app-routing.Module.ts
+++ src/app/app-routing.Module.ts
@@ -2,10 +2,12 @@ import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {AuthorListComponent} from "./components/author-list/author-list.component";
 import {NotFoundComponent} from "./components/not-found/not-found.component";
+import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [
-  { path: 'feed', component: AuthorListComponent },
-  { path: '**', component: NotFoundComponent }
+  { path: 'author/:sortOption', component: AuthorListComponent },
+  { path: '**', component: NotFoundComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
