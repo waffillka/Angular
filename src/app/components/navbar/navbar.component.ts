0356c3f6fed40a56df0013f096ac2a47996aaa90
George Sebik <waffilka@gmail.com>
2021-10-06 12:43:40 +0300

    update nav component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
 
-  constructor() { }
+export class NavbarComponent {
+  isExpanded = false;
 
-  ngOnInit(): void {
+  collapse() {
+    this.isExpanded = false;
   }
 
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
