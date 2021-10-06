b94ac10c8d9d221e1bd256b20cdd99016180af63
George Sebik <waffilka@gmail.com>
2021-10-06 13:25:46 +0300

    added author-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import {RouterModule} from "@angular/router";
 import {AppRoutingModule} from "./app-routing.Module";
 import { AuthorComponent } from './components/author/author.component';
+import { AuthorListComponent } from './components/author-list/author-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AuthorComponent
+    AuthorComponent,
+    AuthorListComponent
   ],
   imports: [
     BrowserModule,
