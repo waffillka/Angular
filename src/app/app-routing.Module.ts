e9fb000d97a63139d96b34109941e4c430c1eb04
George Sebik <waffilka@gmail.com>
2021-10-06 14:31:09 +0300

    added new components and puth

--- src/app/app-routing.Module.ts
+++ src/app/app-routing.Module.ts
@@ -1,10 +1,16 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
+import {AuthorListComponent} from "./components/author-list/author-list.component";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'feed', component: AuthorListComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './components/app/app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import {RouterModule} from "@angular/router";
 import {AppRoutingModule} from "./app-routing.Module";
 import { AuthorComponent } from './components/author/author.component';
 import { AuthorListComponent } from './components/author-list/author-list.component';
+import { AuthorsComponent } from './components/authors/authors.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AuthorComponent,
-    AuthorListComponent
+    AuthorListComponent,
+    AuthorsComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule,
     AppRoutingModule
   ],
   providers: [],
