4985186d3f62e7ddcba4882bbf67dc531c5c93db
George Sebik <waffilka@gmail.com>
2021-10-06 18:26:29 +0300

    added book service

---
+++ src/app/services/book.service.ts
@@ -0,0 +1,65 @@
+import {Injectable} from "@angular/core";
+import {BaseService} from "./base.service";
+import {Book} from "../models/Book";
+import {HttpClient} from "@angular/common/http";
+import {catchError, retry} from "rxjs/operators";
+
+@Injectable()
+export class BookService extends BaseService<Book> {
+  protected path = "book";
+
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  protected getMap(responce: string) {
+    return JSON.parse(responce, (key, value) => {
+      if (key === "date") {
+        return new Date(value);
+      }
+      else {
+        return value;
+      }
+    });
+  }
+
+  public create(snippet: any) {
+    return this.http.post<Book>(`${this.url}/${this.path}/create`, snippet).pipe(
+      catchError(this.HandleError)
+    );
+  }
+
+  public update(book: Book) {
+    return this.http.put<Book>(`${this.url}/${this.path}/update`, book).pipe(
+      catchError(this.HandleError)
+    );
+  }
+
+  public owner(bookId: string) {
+    return this.http.get<boolean>(`${this.url}/is-owner/${bookId}`).pipe(
+      retry(3),
+      catchError(this.HandleError)
+    );
+  }
+
+  public delete(bookId: string) {
+    return this.http.delete(`${this.url}/${this.path}/delete/${bookId}`).pipe(
+      retry(3),
+      catchError(this.HandleError)
+    );
+  }
+
+  public like(bookId:string) {
+    return this.http.post(`${this.url}/like-snippet/${bookId}`, "null").pipe(
+      retry(3),
+      catchError(this.HandleError)
+    );
+  }
+
+  public getLike(bookId:string) {
+    return this.http.get<boolean>(`${this.url}/liked-by/${bookId}`).pipe(
+      retry(3),
+      catchError(this.HandleError)
+    );
+  }
+}
