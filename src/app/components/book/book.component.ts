01b1a84cc9107bcfb00a14f519f0b93261a643f7
George Sebik <waffilka@gmail.com>
2021-10-07 15:52:35 +0300

    added book component some code

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import {BookService} from "../../services/book.service";
+import {Book} from "../../models/Book";
+import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css']
+  styleUrls: ['./book.component.css'],
+  providers: [BookService]
 })
 export class BookComponent implements OnInit {
+  public book: Book | undefined;
+  public bookId: string | undefined;
+  public errorMessage: string | undefined;
 
-  constructor() { }
+  constructor(
+    private service: BookService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.bookId = params.get("bookId")!;
+      this.getSnippet();
+    });
+  }
+
+  public getSnippet() {
+    this.errorMessage = undefined;
+
+    this.service.get(this.bookId!).subscribe(
+      responce => {
+        this.book = responce;
+      },
+      error => {
+        this.errorMessage = error;
+      }
+    );
+  }
+
+  public delete() {
+    this.service.delete(this.bookId!).subscribe(
+      responce => {
+        this.router.navigate(['/profile']);
+      }
+    );
   }
 
 }
