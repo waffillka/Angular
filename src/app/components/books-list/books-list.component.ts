82f63b082399e3065c55e76b245ecad03ebbadc2
George Sebik <waffilka@gmail.com>
2021-10-08 14:03:36 +0300

    update component book-list

--- src/app/components/books-list/books-list.component.ts
+++ src/app/components/books-list/books-list.component.ts
@@ -1,15 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpParams} from "@angular/common/http";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {Book} from "../../models/Book";
+import {BookService} from "../../services/book.service";
 
 @Component({
   selector: 'app-books-list',
   templateUrl: './books-list.component.html',
-  styleUrls: ['./books-list.component.css']
+  styleUrls: ['./books-list.component.css'],
+  providers: [BookService]
 })
 export class BooksListComponent implements OnInit {
+  public books: Book[] | undefined;
+  public errorMessage: string | undefined;
+  public loadingState = false;
 
-  constructor() { }
+  private currPage = 1;
+  private httpParams!: HttpParams;
+
+  constructor(
+    private service: BookService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.books = [];
+      this.currPage = 1;
+      this.setHttpParams(params);
+      this.getBooks();
+    });
   }
 
+  public getBooks() {
+    this.errorMessage = undefined;
+    this.loadingState = true;
+
+    this.service.getMany(this.httpParams).subscribe(
+      responce => {
+        this.books = this.books?.concat(responce);
+        this.httpParams = this.httpParams.set("PageNumber", ++this.currPage);
+        this.loadingState = false;
+      },
+      error => {
+        this.errorMessage = error;
+        this.loadingState = false;
+      }
+    );
+  }
+
+  private setHttpParams(params: ParamMap) {
+    var paramsObject: { [key: string]: any } = {
+      PageNumber: this.currPage,
+      pageSize: 10
+    };
+
+    /*if (params.get('sortOption')) {
+      paramsObject.sortOption = params.get('sortOption');
+    }
+    if (params.get('langName')) {
+      paramsObject.langs = [params.get('langName')];
+    }
+    if (params.get('tagName')) {
+      paramsObject.tags = [params.get('tagName')];
+    }*/
+
+    this.httpParams = new HttpParams({
+      fromObject: paramsObject
+    });
+  }
 }
