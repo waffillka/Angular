da569fee8c487a24b04d2b4c60202bf5c7d6d3e9
George Sebik <waffilka@gmail.com>
2021-10-08 13:58:19 +0300

    update component book short

--- src/app/components/book-short/book-short.component.ts
+++ src/app/components/book-short/book-short.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Book} from "../../models/Book";
 
 @Component({
   selector: 'app-book-short',
   templateUrl: './book-short.component.html',
   styleUrls: ['./book-short.component.css']
 })
-export class BookShortComponent implements OnInit {
+export class BookShortComponent implements OnInit{
+  @Input() book!: Book;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
