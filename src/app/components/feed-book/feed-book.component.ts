e9d13813bb43a358f4acaf1aebd0090c22a2b918
George Sebik <waffilka@gmail.com>
2021-10-08 13:59:01 +0300

    update component feed-book

--- src/app/components/feed-book/feed-book.component.ts
+++ src/app/components/feed-book/feed-book.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {SortLink} from "../../models/SortLink";
 
 @Component({
   selector: 'app-feed-book',
   templateUrl: './feed-book.component.html',
   styleUrls: ['./feed-book.component.css']
 })
-export class FeedBookComponent implements OnInit {
+export class FeedBookComponent {
+  public links: SortLink[] | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
+    this.links = [
+      {
+        optionLink: "/feed/new",
+        optionName: "Новые"
+      },
+      {
+        optionLink: "/feed/popular",
+        optionName: "Популярные"
+      }
+    ];
   }
-
 }
