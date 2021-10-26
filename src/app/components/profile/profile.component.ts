3e3aac5873159d1777cae092a65ed1826db3b771
George Sebik <waffilka@gmail.com>
2021-10-26 12:04:01 +0300

    update ProfileComponent

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {OAuthService} from "angular-oauth2-oidc";
+import {JWTTokenService} from "../../services/JWTTokenService";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers: [JWTTokenService]
 })
 export class ProfileComponent implements OnInit {
-
   constructor(
-
+    public oauthService: OAuthService,
+    public jwtTokenService: JWTTokenService
   ) { }
 
   ngOnInit(): void {
   }
 
+  isAuthenticated(): boolean {
+    return this.oauthService.hasValidAccessToken();
+  }
+
+  public login() {
+    this.oauthService.initLoginFlow();
+  }
+
+  public logout() {
+    this.oauthService.logOut();
+  }
 }
